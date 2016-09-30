30c14ff145f716a7e8f13dbdd647bb5ba0f870f7
SujaySKumar <sujay.skumar141295@gmail.com>
2016-09-30 11:39:11 +0530

    Regularized the input video feed by cropping out the borders and excluding the border contour

--- otsu_first_frame.py
+++ otsu_first_frame.py
@@ -2,13 +2,23 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture('test.mov')
+cap = cv2.VideoCapture('test1.mov')
 while(cap.isOpened()):
     #Read the frame
     ret, frame = cap.read()
 
+    if ret == False:
+        break
+
     #Convert the image to gray
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #Crop the image in 2 vertical halves for videos with front and side view in the same video.
+    y_coords = img1.shape[0]
+    x_coords = img1.shape[1]
+
+    #Leave out 20 pixels in the top to exclude the numbers
+    img = img1[20:y_coords/2, 0:x_coords]
 
     # global thresholding
     #ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
@@ -20,21 +30,29 @@ while(cap.isOpened()):
     blur = cv2.GaussianBlur(img,(5,5),0)
     ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    #To find out the contour with the maximum area. Here this is irrelevant as the haed comes out with
-    #the largest area. Hence get all contours
+    #To find out the contour with the maximum area. The border comes out to be the contour with
+    #the largest area. Hence remove the largest and get all other contours.
+    #Remove contours with area less than 50
     max_area = 0
-    _, contours, hierarchy = cv2.findContours(th3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, hierarchy = cv2.findContours(th3,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     hulls = []
-    # for i in range(len(contours)):
-    #     cnt=contours[i]
-    #     area = cv2.contourArea(cnt)
-    #     hulls.append(cv2.convexHull(cnt))
-    #     if(area>max_area):
-    #         max_area=area
-    #         ci=i
+    areas_to_exclude = []
+    for i in range(len(contours)):
+        cnt=contours[i]
+        area = cv2.contourArea(cnt)
+        if area<50:
+            areas_to_exclude.append(i)
+        hulls.append(cv2.convexHull(cnt))
+        if(area>max_area):
+            max_area=area
+            ci=i
 
     #Contour with highest area
-    #cnt=contours[ci]
+    cnt=contours[ci]
+    areas_to_exclude.append(ci)
+
+    for indices in areas_to_exclude:
+        contours[indices]=None
 
     #Convex hull of the highest area contour
     #hull = cv2.convexHull(cnt)
@@ -44,11 +62,12 @@ while(cap.isOpened()):
     cv2.drawContours(frame,contours,-1,(0,255,0),2)
 
     #Draw all convex hulls
-    cv2.drawContours(frame,hulls,-1,(0,0,255),2)
+    #cv2.drawContours(frame,hulls,-1,(0,0,255),2)
 
 
     cv2.imshow('output',frame)
     cv2.imshow('input', img)
+    cv2.waitKey()
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
